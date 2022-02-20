78a6027508a4bead82d244095c99325076c348ad
Evgengrmit <evgengrmit@gmail.com>
2022-02-21 00:35:57 +0300

    generate code for entries and transfers tables

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -1,1 +1,13 @@
 package db
+
+//
+//import "testing"
+//
+//func TestQueries_CreateAccount(t *testing.T) {
+//	arg := CreateAccountParams{
+//		Owner:    "tom",
+//		Balance:  100,
+//		Currency: "USD",
+//	}
+//	account, err := TestQueries_CreateAccount
+//}
