83044b11607d3cdb9e29c700f1a60634c5457cbe
Evgengrmit <evgengrmit@gmail.com>
2022-02-22 17:03:19 +0300

    add first test

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -1,13 +1,27 @@
 package db
 
-//
-//import "testing"
-//
-//func TestQueries_CreateAccount(t *testing.T) {
-//	arg := CreateAccountParams{
-//		Owner:    "tom",
-//		Balance:  100,
-//		Currency: "USD",
-//	}
-//	account, err := TestQueries_CreateAccount
-//}
+import (
+	"bank/utils"
+	"context"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestCreateAccount(t *testing.T) {
+	req := require.New(t)
+	arg := CreateAccountParams{
+		Owner:    utils.RandomOwner(),
+		Balance:  utils.RandomMoney(),
+		Currency: utils.RandomCurrency(),
+	}
+	account, err := testQueries.CreateAccount(context.Background(), arg)
+	req.NoError(err)
+	req.NotEmpty(account)
+
+	req.Equal(arg.Owner, account.Owner)
+	req.Equal(arg.Balance, account.Balance)
+	req.Equal(arg.Currency, account.Currency)
+
+	req.NotZero(account.ID)
+	req.NotZero(account.CreatedAt)
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.17
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/kyleconroy/sqlc v1.12.0 // indirect
+	github.com/lib/pq v1.10.4 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.3.0 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
